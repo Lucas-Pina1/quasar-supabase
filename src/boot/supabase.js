b98873fb7c510c20de6eba142941da87bfbae348
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-11-11 11:27:06 -0300

    composeble

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qfrvcxdqqzktyhwvykuk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmcnZjeGRxcXprdHlod3Z5a3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgwMDI1MjcsImV4cCI6MTk4MzU3ODUyN30.RQRsiwVi2rXYluLUAQoMgkG6NKtLiojhSmZdIbJ4Hm4'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default function usesupabase () {
+export default function useSupabase () {
   return { supabase }
 }
